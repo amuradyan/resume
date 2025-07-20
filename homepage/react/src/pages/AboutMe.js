e782284802f00d5a3f763083044505316f4da83f
darthspectrum <muradianator@gmail.com>
2025-07-21 02:30:21 +0400

    🏗️refactoring about me. having styling issues

--- homepage/react/src/pages/AboutMe.js
+++ homepage/react/src/pages/AboutMe.js
@@ -1,59 +1,19 @@
 function AboutMe(params) {
   return (
-    <div>
-      <p>
-        I am Andranik, a software engineer based in Yerevan, Armenia, with nearly two decades of programming experience.
-        My background is in computer science, and I am deeply interested in programming languages. I've learned some Czech
-        during my studies abroad and speak Armenian, English, and Russian.
-      </p>
-
-      <p>
-        Most of my industry work is in fintech, although I've also worked in education and travel domains. Projects spanned
-        a variety of tools and languages, and I am now closer to /usually/ typed languages on VMs than the system\hardware.
-        Paired with my opinions on programming languages, that made me better with Scala over, say, Rust, or Python over Go.
-      </p>
-
-      <p>
-        Through trial and error and personal experience, management-wise, I lean towards frequent demos over estimating,
-        pairing over reviews, and managing features over patches. I've noticed that better work is being produced when
-        working in such a fashion.
-      </p>
-
-      <p>
-        In my spare time, I've helped set up learning courses technically /GitHub and Google classrooms, infrastructure,
-        etc./ and content-wise. Every now and then, I'd work on a pet project in a language I was obsessed with at the
-        time /Clojure at the moment/, and once I tried my hand at <a href="https://github.com/haniravi/nito">hardware</a>.
-        On a random note, I've won the only <a
-          href="https://www.seasidestartupsummit.com/blog/nairi-insurance-is-the-first-sponsor-of-the-firecode-marathon">hackathon</a>
-        &nbsp;I ever participated in.
-      </p>
-
-      <p>
-        Generally, I'm interested in programming languages and their ways of describing systems, and lately, dynamic ones,
-        particularly Lisps, have caught my attention, which spilled into a <a href="https://github.com/amuradyan/sea">toy
-          Lisp implementation</a> to experiment with. I'm interested in building systems with dynamic languages to see
-        whether a tight enough test net can take over the type system responsibilities, including the semantics of the
-        latter that aids local reasoning.
-      </p>
-
-      <p>
-        In my early years of studying I was interested in cryptography and information theory, which I still find fascinating.
-        During my PhD years in Charles University in Prague, I took courses in modern programming languages, embedded systems,
-        and system behavior models and verification, but my research was about formal expression and verification /DSL/ of
-        component-based systems.
-      </p>
-
-      <p>
-        If you'd like a dense version of my bio and projects in a PDF format, you can find it <a href="http://amuradyan.am/RESUME.pdf">here</a>.
-      </p>
-
-      <p>
-        If you'd like to drop me a line, you can do so at&nbsp;
-        <a class="email" href="mailto:andranik@amuradyan.am">
-          <b>andranik [at] amuradyan [dot] am</b>
-        </a>, or check out my <a href="http://github.com/amuradyan"><b>GitHub</b></a>.
-      </p>
-    </div >
+    <div className="aboutme-container">
+      <div className="aboutme-left">
+        {/* Replace the src with your actual photo path */}
+        <img
+          src="/me.png"
+          alt="Andranik"
+          className="aboutme-photo"
+        />
+      </div>
+      <div className="aboutme-right">
+        <h2>Hello, I'm Andranik!</h2>
+        {/* All other text is hidden for now */}
+      </div>
+    </div>
   )
 }
 
