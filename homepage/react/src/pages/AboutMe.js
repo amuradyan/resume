ef41c3c2dbbe31b04aa61b40d42ec469c0e20686
darthspectrum <muradianator@gmail.com>
2025-08-02 23:07:19 +0400

    :constuction: acceptable layout

--- homepage/react/src/pages/AboutMe.js
+++ homepage/react/src/pages/AboutMe.js
@@ -1,29 +1,36 @@
 function AboutMe(params) {
   return (
-    <div className="aboutme-container">
-      <div className="aboutme-left">
-        <img
-          src="/me.png"
-          alt="Andranik"
-          className="aboutme-photo"
-        />
-        <div className="aboutme-icons" >
-          <a href="https://github.com/amuradyan" target="_blank" rel="noopener noreferrer" title="GitHub">
-            <img src="./gh.png" alt="GitHub" />
-          </a>
-          <a href="#experience" title="Experience">
-            <img src="./droid.png" alt="Experience" />
-          </a>
-          <a href="mailto:andranik@amuradyan.am" title="Email">
-            <img src="./email.png" alt="Email" />
-          </a>
+    <div className="aboutme-root">
+      <div className="aboutme-top">
+        <div className="aboutme-left">
+          <div className="aboutme-photo-links">
+            <img
+              src="/me.png"
+              alt="Andranik"
+              className="aboutme-photo"
+            />
+            <div className="aboutme-icons">
+              <a href="https://github.com/amuradyan" target="_blank" rel="noopener noreferrer" title="GitHub">
+                <img src="/gh.png" alt="GitHub" />
+              </a>
+              <a href="#experience" title="Experience">
+                <img src="/droid.png" alt="Experience" />
+              </a>
+              <a href="mailto:andranik@amuradyan.am" title="Email">
+                <img src="/email.png" alt="Email" />
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="aboutme-right">
+          <h2>Hello, I'm Andranik!</h2>
+          <p>
+            I enjoy programming /languages/ and write programs both commercially and for fun. Every now and then I'll take up teaching, and when I do not do computer stuff, I try to make things of wood and practice music.
+          </p>
         </div>
       </div>
-      <div className="aboutme-right">
-        <h2>Hello, I'm Andranik!</h2>
-        <p>
-          I enjoy programming /languages/ and write programs both commercially and for fun. When not at the computer I try to make things of wood and practice music.
-        </p>
+      <div className="aboutme-bottom">
+        <h2>Projects</h2>
       </div>
     </div>
   )
